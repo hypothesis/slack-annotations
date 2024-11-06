2711e7c9d2218d31e2ec93e757dd7bea3312493e
Sean Hammond <seanh@users.noreply.github.com>
2024-11-06 17:03:54 +0000

    Formatting

--- src/slack_annotations/core.py
+++ src/slack_annotations/core.py
@@ -32,38 +32,34 @@ def notify(group=None, token=None, cache_path=None):
             json.dump({"search_after": annotations[-1]["created"]}, cache_file)
 
     def format_annotation(annotation):
-        block = {
-            "type": "section",
-            "text": {
-                "text": "Someone posted an annotation:",
-                "type": "mrkdwn"
+        blocks = [
+            {
+                "type": "section",
+                "text": {"text": "Someone posted an annotation:", "type": "mrkdwn"},
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*{annotation['user_info']['display_name']}* `{annotation['user']}`",
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*{annotation['document']['title']} {annotation['uri']}",
+                    },
+                    {"type": "plain_text", "text": f"*{annotation['text']}*"},
+                ],
             },
-            "fields": [
-                {
-                    "type": "mrkdwn",
-                    "text": f"*{annotation['user_info']['display_name']}* `{annotation['user']}`"
-                },
-                {
-                    "type": "mrkdwn",
-                    "text": f"*{annotation['document']['title']} {annotation['uri']}"
-                },
-                {
-                    "type": "plain_text",
-                    "text": f"*{annotation['text']}*"
-                },
-            ]
-        }
+            {"type": "divider"},
+        ]
 
         if annotation["tags"]:
-            block["fields"].append(
+            blocks[0]["fields"].append(
                 {
                     "type": "mrkdwn",
-                    "text": ",".join(f"`{tag}`" for tag in annotation["tags"])
+                    "text": ",".join(f"`{tag}`" for tag in annotation["tags"]),
                 },
             )
 
-        return block
-
+        return blocks
 
     if annotations:
         if len(annotations) == 1:
@@ -71,11 +67,11 @@ def notify(group=None, token=None, cache_path=None):
         else:
             summary = f"{len(annotations)} new annotations were psoted"
 
-        return json.dumps({
-            "text": summary,
-            "blocks": [
-                format_annotation(annotation) for annotation in annotations
-            ]
-        })
+        return json.dumps(
+            {
+                "text": summary,
+                "blocks": [*format_annotation(annotation) for annotation in annotations],
+            }
+        )
 
     return ""
