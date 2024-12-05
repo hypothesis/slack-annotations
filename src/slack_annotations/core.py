30b68d603584d99697435447f90a762c43b0b25f
Misha Tomilov <mtomilov@gmail.com>
2024-12-05 18:48:01 +0100

    Refacotr core.notify

--- src/slack_annotations/core.py
+++ src/slack_annotations/core.py
@@ -1,135 +1,163 @@
 import json
 from datetime import UTC, datetime, timedelta
+from typing import Any
 
 import httpx
 
 SEARCH_HOURS = 1
+MAX_TEXT_LENGTH = 2000
 
 
 def notify(
-    search_params=None, token=None, cache_path=None
-):  # pylint:disable=too-complex,too-many-statements # pragma:no cover
-
-    search_params = search_params or {}
-
-    # Deliberately override any given sort or order param as these specific
-    # values are needed for the algorithm below to work.
-    search_params["sort"] = "created"
-    search_params["order"] = "asc"
-    search_params["search_after"] = (
-        datetime.now(UTC) - timedelta(hours=SEARCH_HOURS)
-    ).isoformat()
-
+    search_params: dict[str, Any] | None = None,
+    token: str | None = None,
+    cache_path: str | None = None,
+):
+    search_params = _init_search_params(search_params or {})
     if cache_path:
-        try:
-            with open(cache_path, "r", encoding="utf-8") as cache_file:
-                search_params["search_after"] = max(
-                    search_params["search_after"], json.load(cache_file)["search_after"]
-                )
-        except FileNotFoundError:
-            pass
-
+        search_params["search_after"] = _get_search_after(
+            cache_path, search_params["search_after"]
+        )
     headers = {}
-
     if token:
         headers["Authorization"] = f"Bearer {token}"
 
-    annotations = (
-        httpx.get(
-            "https://hypothes.is/api/search", params=search_params, headers=headers
-        )
+    annotations = _fetch_annotations(search_params, headers)
+    if annotations and cache_path:
+        _update_cache(cache_path, annotations[-1]["created"])
+
+    return _format_annotations(annotations)
+
+
+def _init_search_params(params: dict[str, Any]) -> dict[str, Any]:
+    # Deliberately override any given sort or order param as these specific
+    # values are needed for the algorithm below to work.
+    params.update(
+        {
+            "sort": "created",
+            "order": "asc",
+            "search_after": (
+                datetime.now(UTC) - timedelta(hours=SEARCH_HOURS)
+            ).isoformat(),
+        }
+    )
+    return params
+
+
+def _get_search_after(cache_path: str, default: str) -> str:
+    try:
+        with open(cache_path, "r", encoding="utf-8") as f:
+            return max(default, json.load(f)["search_after"])
+    except FileNotFoundError:
+        pass
+    return default
+
+
+def _update_cache(cache_path: str, search_after: str) -> None:
+    with open(cache_path, "w", encoding="utf-8") as f:
+        json.dump({"search_after": search_after}, f)
+
+
+def _fetch_annotations(
+    params: dict[str, Any], headers: dict[str, str]
+) -> list[dict[str, Any]]:
+    return (
+        httpx.get("https://hypothes.is/api/search", params=params, headers=headers)
         .raise_for_status()
         .json()["rows"]
     )
 
-    if annotations and cache_path:
-        with open(cache_path, "w", encoding="utf-8") as cache_file:
-            json.dump({"search_after": annotations[-1]["created"]}, cache_file)
 
-    def format_annotation(annotation):
-        def get_quote(annotation):
-            for target in annotation["target"]:
-                for selector in target["selector"]:
-                    if exact := selector.get("exact"):
-                        if not exact:
-                            exact = "(None)"
-                        if len(exact) > 2000:
-                            exact = exact[:1997] + "..."
-                        return exact
+def _format_annotation(annotation: dict[str, Any]) -> dict[str, Any]:
+    fields = _build_annotation_fields(annotation)
+    summary = _build_annotation_summary(annotation)
+    return {
+        "type": "section",
+        "text": {"type": "mrkdwn", "text": summary},
+        "fields": fields,
+    }
 
-            raise ValueError()
 
-        def get_text(annotation):
-            text = annotation.get("text", None)
+def _get_quote(annotation: dict[str, Any]) -> str:
+    for target in annotation["target"]:
+        for selector in target["selector"]:
+            if exact := selector.get("exact"):
+                if not exact:
+                    exact = "(None)"
+                return _trim_text(exact)
+    raise ValueError()
 
-            if not text:
-                text = "(None)"
 
-            if len(text) > 2000:
-                text = text[:1997] + "..."
-            return text
+def _trim_text(text: str) -> str:
+    stub = "..."
+    if len(text) > MAX_TEXT_LENGTH:
+        return text[: MAX_TEXT_LENGTH - len(stub)] + stub
+    return text
 
-        try:
-            quote = get_quote(annotation)
-        except:  # pylint:disable=bare-except
-            quote = "(None)"
 
-        try:
-            title = annotation["document"]["title"][0]
-        except:  # pylint:disable=bare-except
-            title = None
+def _get_text(annotation: dict[str, Any]) -> str:
+    text = annotation.get("text", None)
+    if not text:
+        text = "(None)"
+    return _trim_text(text)
 
-        fields = [
-            {"type": "mrkdwn", "text": "*Quote:*"},
-            {
-                "type": "mrkdwn",
-                "text": f"*Annotation* (<{annotation['links']['incontext']}|in-context link>):",
-            },
-            {"type": "plain_text", "text": quote},
-            {"type": "plain_text", "text": get_text(annotation)},
-        ]
 
-        display_name = annotation["user_info"]["display_name"]
-        username = annotation["user"].split(":")[1].split("@")[0]
-        uri = annotation["uri"]
+def _format_annotations(annotations: list[dict[str, Any]]) -> str:
+    if not annotations:
+        return ""
 
-        if title:
-            document_link = f"<{uri}|{title}>"
-        else:
-            document_link = uri
+    if len(annotations) == 1:
+        summary = "A new annotation was posted"
+    else:
+        summary = f"{len(annotations)} new annotations"
 
-        summary = f"`{username}` ({display_name}) annotated {document_link}:"
-
-        return {
-            "type": "section",
-            "text": {"type": "mrkdwn", "text": summary},
-            "fields": fields,
+    blocks = []
+    for annotation in annotations:
+        blocks.append(_format_annotation(annotation))
+        blocks.append({"type": "divider"})
+    blocks.append(
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "mrkdwn",
+                    "text": "These annotations are posted to Slack by a <https://github.com/hypothesis/slack-annotations/|GitHub Actions workflow>",
+                },
+            ],
         }
+    )
 
-    if annotations:
-        if len(annotations) == 1:
-            summary = "A new annotation was posted"
-        else:
-            summary = f"{len(annotations)} new annotations"
+    return json.dumps({"text": summary, "blocks": blocks})
 
-        blocks = []
 
-        for annotation in annotations:
-            blocks.append(format_annotation(annotation))
-            blocks.append({"type": "divider"})
+def _build_annotation_summary(annotation: dict[str, Any]) -> str:
+    username = annotation["user"].split(":")[1].split("@")[0]
+    display_name = annotation["user_info"]["display_name"]
+    uri = annotation["uri"]
 
-        blocks.append(
-            {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": "These annotations are posted to Slack by a <https://github.com/hypothesis/slack-annotations/|GitHub Actions workflow>",
-                    },
-                ],
-            }
-        )
+    try:
+        title = annotation["document"]["title"][0]
+    except Exception:  # pylint:disable=broad-exception-caught
+        title = None
+    if title:
+        document_link = f"<{uri}|{title}>"
+    else:
+        document_link = uri
+    return f"`{username}` ({display_name}) annotated {document_link}:"
 
-        return json.dumps({"text": summary, "blocks": blocks})
 
-    return ""
+def _build_annotation_fields(annotation: dict[str, Any]) -> list[dict[str, Any]]:
+    try:
+        quote = _get_quote(annotation)
+    except Exception:  # pylint:disable=broad-exception-caught
+        quote = "(None)"
+
+    return [
+        {"type": "mrkdwn", "text": "*Quote:*"},
+        {
+            "type": "mrkdwn",
+            "text": f"*Annotation* (<{annotation['links']['incontext']}|in-context link>):",
+        },
+        {"type": "plain_text", "text": quote},
+        {"type": "plain_text", "text": _get_text(annotation)},
+    ]
