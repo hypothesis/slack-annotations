fe53caf6fbfdcd4d03b0e05f0940954c52fc0320
Misha Tomilov <mtomilov@gmail.com>
2024-12-05 19:15:09 +0100

    Add unit test_init_search_params

--- tests/unit/slack_annotations/core_test.py
+++ tests/unit/slack_annotations/core_test.py
@@ -1,3 +1,12 @@
-class TestHelloWorld:
-    def test_it(self):
-        pass
+from datetime import datetime, timedelta, UTC
+from src.slack_annotations.core import _init_search_params, SEARCH_HOURS
+
+
+def test_init_search_params():
+    params = _init_search_params({"some_key": "some_value"})
+    assert params == {
+        "some_key": "some_value",
+        "sort": "created",
+        "order": "asc",
+        "search_after": (datetime.now(UTC) - timedelta(hours=SEARCH_HOURS)).isoformat(),
+    }
