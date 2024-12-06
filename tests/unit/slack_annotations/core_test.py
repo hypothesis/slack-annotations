41d0bc8600a77544f9fe66186424a66f4946f27d
Misha Tomilov <mtomilov@gmail.com>
2024-12-06 13:25:33 +0100

    Add unit test_get_quote_without_exact

--- tests/unit/slack_annotations/core_test.py
+++ tests/unit/slack_annotations/core_test.py
@@ -9,6 +9,7 @@ from slack_annotations.core import (
     SEARCH_HOURS,
     _fetch_annotations,
     _format_annotations,
+    _get_quote,
     _get_search_after,
     _init_search_params,
     _update_cache,
@@ -109,8 +110,15 @@ def test_notify_with_search_after_from_cache_file(
 
     assert notify(cache_path=str(cache_path)) == json.dumps(slack_annotations)
     assert json.loads(cache_path.read_text()) == {
-        "search_after": "2024-12-02T18:34:42.333087+00:00"
+        "search_after": "2024-12-03T18:40:42.325652+00:00"
     }
+
+
+def test_get_quote_without_exact():
+    annotation = {"target": [{"selector": [{}]}]}
+
+    with pytest.raises(ValueError):
+        _get_quote(annotation)
 
 
 @pytest.fixture
@@ -122,16 +130,19 @@ def slack_annotations():
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "`test_user_1` (md............................) annotated <https://example.com/|Annotating the law | Hypothes.is>:",
+                    "text": "`test_user_2` (Test User 2) annotated <https://web.hypothes.is/blog/step-by-step-guide-to-using-hypothesis-for-collaborative-projects/|Annotating the law | Hypothes.is>:",
                 },
                 "fields": [
                     {"type": "mrkdwn", "text": "*Quote:*"},
                     {
                         "type": "mrkdwn",
-                        "text": "*Annotation* (<https://hyp.is/test_annotation_id_1/example.com/|in-context link>):",
+                        "text": "*Annotation* (<https://hyp.is/test_annotation_id_2/web.hypothes.is/blog/step-by-step-guide-to-using-hypothesis-for-collaborative-projects/|in-context link>):",
                     },
-                    {"type": "plain_text", "text": "(None)"},
-                    {"type": "plain_text", "text": "test_user_1 reply"},
+                    {
+                        "type": "plain_text",
+                        "text": "The ability to collaborate effectively is invaluable in today\u2019s interconnected world. Whether in academic or professional settings, tools that streamline communication, enhance content sharing and promote open dialogue are important. Hypothesis is a standout tool in this regard, especially renowned for its capabilities in group annotations and as an online learning tool.",
+                    },
+                    {"type": "plain_text", "text": "A useful tool!"},
                 ],
             },
             {"type": "divider"},
@@ -153,15 +164,25 @@ def search_annotations():
     return {
         "rows": [
             {
-                "created": "2024-12-02T18:34:42.333087+00:00",
-                "user": "acct:test_user_1@hypothes.is",
-                "uri": "https://example.com/",
-                "text": "test_user_1 reply",
+                "created": "2024-12-03T18:40:42.325652+00:00",
+                "user": "acct:test_user_2@hypothes.is",
+                "uri": "https://web.hypothes.is/blog/step-by-step-guide-to-using-hypothesis-for-collaborative-projects/",
+                "text": "A useful tool!",
+                "target": [
+                    {
+                        "source": "https://web.hypothes.is/blog/step-by-step-guide-to-using-hypothesis-for-collaborative-projects/",
+                        "selector": [
+                            {
+                                "exact": "The ability to collaborate effectively is invaluable in today’s interconnected world. Whether in academic or professional settings, tools that streamline communication, enhance content sharing and promote open dialogue are important. Hypothesis is a standout tool in this regard, especially renowned for its capabilities in group annotations and as an online learning tool."
+                            },
+                        ],
+                    }
+                ],
                 "document": {"title": ["Annotating the law | Hypothes.is"]},
                 "links": {
-                    "incontext": "https://hyp.is/test_annotation_id_1/example.com/"
+                    "incontext": "https://hyp.is/test_annotation_id_2/web.hypothes.is/blog/step-by-step-guide-to-using-hypothesis-for-collaborative-projects/"
                 },
-                "user_info": {"display_name": "md............................"},
+                "user_info": {"display_name": "Test User 2"},
             },
         ]
     }
