72577a2f619df57bdc4df18475bb88da976803c6
Misha Tomilov <mtomilov@gmail.com>
2024-12-09 17:41:21 +0100

    Clean up

--- tests/unit/slack_annotations/format_test.py
+++ tests/unit/slack_annotations/format_test.py
@@ -34,40 +34,14 @@ class TestFormatAnnotation:
             "user": "acct:test_user_1@hypothes.is",
             "uri": "https://example.com/",
             "links": {"incontext": "https://hyp.is/test_annotation_id_1/example.com/"},
-            "user_info": {"display_name": "md............................"},
-        }
-
-        assert _format_annotation(annotation) == {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "`test_user_1` (md............................) annotated https://example.com/:",
-            },
-            "fields": [
-                {"type": "mrkdwn", "text": "*Quote:*"},
-                {
-                    "type": "mrkdwn",
-                    "text": "*Annotation* (<https://hyp.is/test_annotation_id_1/example.com/|in-context link>):",
-                },
-                {"type": "plain_text", "text": "(None)"},
-                {"type": "plain_text", "text": "(None)"},
-            ],
-        }
-
-    def test_page_note_without_user_display_name(self):
-        annotation = {
-            "user": "acct:test_user_1@hypothes.is",
-            "uri": "https://example.com/",
-            "links": {"incontext": "https://hyp.is/test_annotation_id_1/example.com/"},
             "user_info": {"display_name": None},
-            "document": {"title": ["Example annotation"]},
         }
 
         assert _format_annotation(annotation) == {
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": "`test_user_1` annotated <https://example.com/|Example annotation>:",
+                "text": "`test_user_1` annotated https://example.com/:",
             },
             "fields": [
                 {"type": "mrkdwn", "text": "*Quote:*"},
